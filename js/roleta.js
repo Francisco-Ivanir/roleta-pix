362384d42b7ba16baa096685ba78db0fce2081b1
Francisco-Ivanir <ficfivanir@outlook.com>
2026-07-11 18:58:27 -0300

    Update roleta.js

--- js/roleta.js
+++ js/roleta.js
@@ -294,3 +294,40 @@ document
 
 });
 
+function abrirModalPix(){
+
+  document
+  .getElementById("modalPix")
+  .style.display = "flex";
+
+}
+
+document
+.getElementById("btnFecharPix")
+.addEventListener("click",()=>{
+
+
+document
+.getElementById("modalPix")
+.style.display="none";
+
+
+});
+
+document
+.getElementById("btnJaPaguei")
+.addEventListener("click",()=>{
+
+
+giroLiberado = true;
+
+
+document
+.getElementById("modalPix")
+.style.display="none";
+
+
+alert("Pagamento confirmado! Giro liberado.");
+
+
+});
