f43aa9be70a0afc7ca6032039d72d21ab2664d46
Francisco-Ivanir <ficfivanir@outlook.com>
2026-07-12 14:16:12 -0300

    Update roleta.js

--- js/roleta.js
+++ js/roleta.js
@@ -72,6 +72,13 @@ let girando = false;
 let giroLiberado = false;
 let pagamentoAtual = null;
 
+let clienteAtual = {
+
+  nome:"",
+  whatsapp:""
+
+};
+
 const pixConfig = {
 
   chave:
@@ -258,7 +265,7 @@ document
 
 if (!giroLiberado) {
 
-  abrirModalPix();
+  abrirModalCliente();
 
   return;
 
@@ -505,8 +512,74 @@ console.error(
 erro
 );
 
+  function abrirModalCliente(){
+
+document
+.getElementById("modalCliente")
+.style.display="flex";
+
+}
+  
+
 
 }
 
 
 }
+
+document
+.getElementById("btnContinuarCliente")
+.addEventListener("click",()=>{
+
+
+let nome =
+document
+.getElementById("nomeCliente")
+.value;
+
+
+let whatsapp =
+document
+.getElementById("whatsappCliente")
+.value;
+
+
+whatsapp =
+whatsapp.replace(/\D/g,"");
+
+
+if(whatsapp.length < 10){
+
+alert("Digite um WhatsApp válido.");
+
+return;
+
+}
+
+
+clienteAtual.nome = nome;
+
+clienteAtual.whatsapp = whatsapp;
+
+
+document
+.getElementById("modalCliente")
+.style.display="none";
+
+
+abrirModalPix();
+
+
+});
+
+document
+.getElementById("btnFecharCliente")
+.addEventListener("click",()=>{
+
+
+document
+.getElementById("modalCliente")
+.style.display="none";
+
+
+});
