342bad2f4351baf552a88cc14f8645ae3a34abef
Francisco-Ivanir <ficfivanir@outlook.com>
2026-07-12 20:12:59 -0300

    Update cliente.js

--- js/cliente.js
+++ js/cliente.js
@@ -12,3 +12,61 @@ function abrirModalCliente(){
   .style.display = "flex";
 
 }
+
+document
+.getElementById("btnContinuarCliente")
+.addEventListener("click",()=>{
+
+
+let nome =
+document
+.getElementById("nomeCliente")
+.value;
+
+
+let whatsapp =
+document
+.getElementById("whatsappCliente")
+.value;
+
+
+whatsapp =
+whatsapp.replace(/\D/g,"");
+
+
+if(whatsapp.length < 10){
+
+alert("Digite um WhatsApp válido.");
+
+return;
+
+}
+
+
+clienteAtual.nome = nome;
+
+clienteAtual.whatsapp = whatsapp;
+
+
+document
+.getElementById("modalCliente")
+.style.display="none";
+
+
+abrirModalPix();
+
+
+});
+
+document
+.getElementById("btnFecharCliente")
+.addEventListener("click",()=>{
+
+
+document
+.getElementById("modalCliente")
+.style.display="none";
+
+
+});
+
