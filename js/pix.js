b0d6f0c124de27fac51ed2177159ef9a3e7139c3
Francisco-Ivanir <ficfivanir@outlook.com>
2026-07-13 10:32:52 -0300

    Update pix.js

--- js/pix.js
+++ js/pix.js
@@ -15,7 +15,7 @@ let pagamentoAtual = null;
 const pixConfig = {
 
   chave:
-  "SUA_CHAVE_AQUI",
+  "00020126580014br.gov.bcb.pix01364414c932-5e48-4188-a572-7291c6cbb83a5204000053039865802BR5901N6001C62090505648266304230B",
 
   valor:
   1.00
