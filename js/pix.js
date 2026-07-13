706150d6808514d3b1ad133e10096c7754be955d
Francisco-Ivanir <ficfivanir@outlook.com>
2026-07-12 21:39:58 -0300

    Update pix.js

--- js/pix.js
+++ js/pix.js
@@ -1,2 +1,24 @@
+import { db } from "../config/firebase-config.js";
 
+import {
+  collection,
+  addDoc,
+  updateDoc,
+  doc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+let pagamentoAtual = null;
+
+
+const pixConfig = {
+
+  chave:
+  "SUA_CHAVE_AQUI",
+
+  valor:
+  1.00
+
+};
 
