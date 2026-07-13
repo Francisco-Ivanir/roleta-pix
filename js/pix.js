cd9a09c95cf3ebeb6b4168bd7b522f5cc1dae132
Francisco-Ivanir <ficfivanir@outlook.com>
2026-07-13 10:44:14 -0300

    Update pix.js

--- js/pix.js
+++ js/pix.js
@@ -22,3 +22,206 @@ const pixConfig = {
 
 };
 
+export async function abrirModalPix(){
+
+
+document
+.getElementById("modalPix")
+.style.display = "flex";
+
+
+document
+.getElementById("chavePix")
+.innerText =
+pixConfig.chave;
+
+
+document
+.getElementById("valorPix")
+.innerText =
+"R$ " +
+pixConfig.valor.toFixed(2)
+.replace(".",",");
+
+
+
+try {
+
+
+const pagamento =
+await addDoc(
+collection(db,"pagamentos"),
+{
+
+valor: pixConfig.valor,
+
+status:"pendente",
+
+criadoEm:
+serverTimestamp()
+
+});
+
+
+pagamentoAtual = pagamento.id;
+
+
+console.log(
+"Pagamento criado:",
+pagamento.id
+);
+
+
+
+}
+catch(erro){
+
+console.error(
+"Erro pagamento:",
+erro
+);
+
+
+}
+
+
+}
+
+document
+.getElementById("btnFecharPix")
+.addEventListener("click",()=>{
+
+
+document
+.getElementById("modalPix")
+.style.display="none";
+
+
+});
+
+document
+.getElementById("btnJaPaguei")
+.addEventListener("click",async ()=>{
+
+
+if(pagamentoAtual){
+
+
+await updateDoc(
+
+doc(
+db,
+"pagamentos",
+pagamentoAtual
+),
+
+{
+
+status:"confirmado",
+
+confirmadoEm:
+serverTimestamp()
+
+}
+
+);
+
+
+console.log(
+"Pagamento confirmado:",
+pagamentoAtual
+);
+
+
+}
+
+
+giroLiberado = true;
+
+
+document
+.getElementById("modalPix")
+.style.display="none";
+
+
+alert("Pagamento confirmado! Giro liberado.");
+
+
+});
+
+document
+.getElementById("btnCopiarPix")
+.addEventListener("click",()=>{
+
+
+navigator.clipboard.writeText(
+pixConfig.chave
+);
+
+
+alert("Chave PIX copiada!");
+
+});
+
+async function finalizarPagamento(premioSorteado){
+
+
+if(!pagamentoAtual){
+
+console.log("Nenhum pagamento encontrado.");
+
+return;
+
+}
+
+
+try{
+
+
+await updateDoc(
+
+doc(
+db,
+"pagamentos",
+pagamentoAtual
+),
+
+{
+
+status:"finalizado",
+
+premio:
+premioSorteado.nome,
+
+peso:
+premioSorteado.peso,
+
+finalizadoEm:
+serverTimestamp()
+
+}
+
+);
+
+
+console.log(
+"Pagamento finalizado:",
+pagamentoAtual
+);
+
+
+}
+
+
+catch(erro){
+
+console.error(
+"Erro ao finalizar:",
+erro
+);
+
+
+}
+
+
+}
