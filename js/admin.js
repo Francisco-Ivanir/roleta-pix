0bc31dd5112c21bd4590a3f701cf028290f6435d
Francisco-Ivanir <ficfivanir@outlook.com>
2026-07-20 23:15:36 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -2,6 +2,8 @@ import { db } from "../config/firebase-config.js";
 
 import {
   collection,
+  query,
+  orderBy,
   onSnapshot
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
